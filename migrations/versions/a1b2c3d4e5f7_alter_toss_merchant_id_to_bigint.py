2a697222f922e80fc5b348f94e333683b7112271
whrksp126 <whrksp126@gmail.com>
2026-03-21 15:09:32 +0900

    fix: fix migration chain - set down_revision to d1e2f3a4b5c6

--- migrations/versions/a1b2c3d4e5f7_alter_toss_merchant_id_to_bigint.py
+++ migrations/versions/a1b2c3d4e5f7_alter_toss_merchant_id_to_bigint.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'a1b2c3d4e5f7'
-down_revision = '08f669ab058f'
+down_revision = 'd1e2f3a4b5c6'
 branch_labels = None
 depends_on = None
 
